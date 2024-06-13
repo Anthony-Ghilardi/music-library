287f5a59df0076fb954e212318e0c2719c6ba4d1
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-06-12 21:29:16 -0700

    Interface imporvements wip

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -3,14 +3,35 @@ import { useParams } from 'react-router-dom'
 
 function ArtistView() {
     const { id } = useParams()
-    const [ artistData, setArtistData ]= useState([])
+    const [ artistData, setArtistData ] = useState([])
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key="justAlbums">
+                <p>{album.collectionName}</p>
+            </div>
+        )
+    })
+    
     return (
         <div>
             <h2>The id passed was: {id}</h2>
             <p>Artist Data Goes Here!</p>
+            {renderAlbums}
         </div>
     )
+    
 }
 
 export default ArtistView
